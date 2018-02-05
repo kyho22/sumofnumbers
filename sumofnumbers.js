48ab4e21183f19ecbeb7f05a2bc63a9cc3b60cd0
kyho22 <kyho35@hawaii.edu>
2018-02-04 21:04:21 -1000

    Finished Excercise

---
+++ sumofnumbers.js
@@ -0,0 +1,37 @@
+function sumWhile(list) {
+  let i = 0;
+  let sum = 0;
+  while (i < list.length) {
+    sum += list[i];
+    i++;
+  }
+  return sum;
+}
+
+const alist = [1, 2, 3];
+console.log(sumWhile(alist));
+
+function sumFor(list) {
+  let sum = 0;
+  for (let i = 0; i < list.length; i++) {
+    sum += list[i];
+  }
+  return sum;
+}
+
+console.log(sumFor(alist));
+
+function sumTheSimpleWay(list) {
+  return _.reduce(list, function (memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumTheSimpleWay(alist));
+
+function sumRecursion(list) {
+  if (list.length === 0) {
+    return 0;
+  }
+  return list[0] + sumRecursion(list.slice(1, list.length));
+}
+
+console.log(sumRecursion(alist));
